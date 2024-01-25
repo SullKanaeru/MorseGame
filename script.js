a35191ba78edaf1fd33bd0bfc3c5210bc53a1bbc
SullKanaeru <zulhanarif77@gmail.com>
2024-01-25 22:58:39 +0700

    update 2.7

--- script.js
+++ script.js
@@ -1,8 +1,61 @@
-const container = document.querySelector(".container");
-container.style.display = 'none';
 
-//Declaration for game
-const words = ["capcipcupcepcop", "lambemuilodek", "kapalkecilayah", "technofestasik", "averosfair2024", "janganlupabahagia", "badebombumbimbam", "hahihuhehohau"];
+const gamepage = document.querySelector(".gamepage");
+const gas_btn = document.querySelector('.btn_homeToGame');
+const homepage = document.querySelector('.homepage');
+const body = document.querySelector('body');
+const dataZulhan = document.querySelector('.fotoku');
+const profilpage = document.querySelector('.profilpage');
+
+homepage.style.display = 'flex';
+gamepage.style.display = 'none';
+profilpage.style.display = 'none';
+
+//Settingan untuk perubahan tampilan di website
+//Function untuk memunculkan halaman game
+function homeToGame(){
+    homepage.style.display = 'none';
+    gamepage.style.display = 'flex';
+    body.style.overflowY = 'visible';
+    body.style.overflowX = 'none';
+
+    if(interval){
+        return
+    }
+    interval = setInterval(timer, 1000);
+}
+
+//Function untuk memunculkan halaman profil developer
+function portofolioku() {
+    dataZulhan.style.display = 'flex';
+    homepage.style.display = 'none';
+    profilpage.style.display = 'flex';
+    body.style.overflow = 'none';
+}
+
+//Function untuk memunculkan kembali ke halaman beranda
+function backtohome() {
+    dataZulhan.style.display = 'none';
+    homepage.style.display = 'flex';
+    profilpage.style.display = 'none';
+    body.style.overflow = 'none';
+}
+
+//
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Script untuk game
+const kumpulanKata = [
+    "Microprocessor",
+    "Motherboard",
+    "GraphicsCard",
+    "SolidStateDrive",
+    "MotherMemory",
+    "CentralUnit",
+    "PowerSupply",
+    "InputOutput",
+    "Peripherals",
+    "Configuration"
+];
+
 const morseCode = {
     "a": ".-",
     "b": "-...",
@@ -42,59 +95,48 @@ const morseCode = {
     "0": "-----"
 };
 
+// Mengambil kata sembarang di array words dan ditampilkan di class word
+let kataTerpilih = ambilKataRandom();
+let kataRandom = document.getElementById("kataRandom");
+kataRandom.innerHTML = kataTerpilih;
 
-//Script untuk game
-// function untuk kata random dari array words
-function getRandomWord() {
-    return words[Math.floor(Math.random() * words.length)];
+// Mengambil jawaban
+let answer = ambilKodeMorse(kataTerpilih);
+
+// function untuk mengambil kata random dari array kumpulanKata
+function ambilKataRandom() {
+    return kumpulanKata[Math.floor(Math.random() * kumpulanKata.length)];
 }
 
 // function untuk menentukan apa kode morse untuk kata yang didapatkan 
-function getMorseCode(word) {
+function ambilKodeMorse(kataTerpilih) {
     let morseCodeArr = [];
-    for (let i = 0; i < word.length; i++) {
-        if (morseCode[word[i]]) {
-            morseCodeArr.push(morseCode[word[i]]);
+    for (let i = 0; i < kataTerpilih.length; i++) {
+        if (morseCode[kataTerpilih[i]]) {
+            morseCodeArr.push(morseCode[kataTerpilih[i]]);
         }
     }
     return morseCodeArr.join(" ");
 }
 
-
-// Function untuk cek kebenaran jawaban
-function checkAnswer(input, answer) {
-    return input.trim() === answer.trim();
-}
-
-// Function untuk update DOM
-function updateDOM(result) {
-    let resultElement = document.getElementById("result");
-    resultElement.innerHTML = result;
-}
-
-// Mengambil kata sembarang di array words dan ditampilkan di class word
-let word = getRandomWord();
-let wordElement = document.getElementById("word");
-wordElement.innerHTML = word;
-
-// Mengambil jawaban
-let answer = getMorseCode(word);
-
 // Function agar user dapat click button untuk periksa jawaban
-let submitButton = document.getElementById("submit");
-let hasil = document.querySelector(".game");
-let valuehasil = document.createElement("p");
+let btn_submit = document.getElementById("submit");
+let hasil = document.querySelector(".gamecontent");
+let valuehasil = document.querySelector("#result");
 
-submitButton.addEventListener("click", function() {
+btn_submit.addEventListener("click", function() {
     // Mengambil input user
     let input = document.getElementById("input").value;
+
     // cek jawaban
-    let isCorrect = checkAnswer(input, answer);
+    let isCorrect = cekJawaban(input, answer);
+
     // Update DOM berdasarkan kebenaran jawaban
     if (isCorrect) {
         valuehasil.classList.add("valuehasilbenar"); 
         valuehasil.textContent = "Jawaban anda benar! Selamatttt";
         hasil.appendChild(valuehasil);
+
         // Menunda reload selama 5 detik
         setTimeout(function() {
             location.reload();
@@ -102,20 +144,25 @@ submitButton.addEventListener("click", function() {
         }, 4000);
     } else {
         valuehasil.classList.add("valuehasilsalah"); 
-        valuehasil.textContent = "Jawaban anda masih salah ni sam! coba cek lagi";
-        hasil.appendChild(valuehasil);
+        valuehasil.textContent = "Jawaban kamu salah, coba periksa kembali :)";
+        hasil.appendChild(valuehasil);  
     }
 });
 
+// Function untuk cek kebenaran jawaban
+function cekJawaban(input, answer) {
+    return input.trim() === answer.trim();
+}
 
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //Settingan waktu -> Di sini apabila ingin mensetting waktu
-const time_el = document.querySelector('.watch .time');
-const watchBox = document.querySelector('.watch');
-const start_btn = document.getElementById('#start');
-const stop_btn = document.getElementById('#stop');
+const time = document.querySelector(".time");
+const watchBox = document.querySelector(".watch");
+const start_btn = document.getElementById("#start");
+const stop_btn = document.getElementById("#stop");
 
 //Script untuk timer
-
 let seconds = 120; // -> ubah bagian ini untuk mengubah batas waktu timer
 let interval = null;
 
@@ -127,8 +174,7 @@ function timer(){
     let secs = seconds % 60;
 
     if(secs < 10){
-        secs = '0' + secs;
-
+        secs = "0" + secs;
     }
 
     if(mins < 10){
@@ -148,48 +194,11 @@ function timer(){
         location.reload();
     }
     
-
-    time_el.innerText = `${mins}:${secs}`;
+    time.textContent = `${mins}:${secs}`;
 };
 
 
-//Settingan untuk perubahan tampilan di website
-const gas_btn = document.querySelector('.gas');
-const main_page = document.querySelector('.mainmenu');
-const all_page = document.querySelector('.container');
-const body = document.querySelector('body');
-const dataZulhan = document.querySelector('.fotoku');
-const biodeveloper = document.querySelector('.infoPembuat');
-
-
-//Function untuk memunculkan halaman game
-function gas(){
-    main_page.style.display = 'none';
-    all_page.style.display = 'flex';
-    body.style.overflowY = 'visible';
-    body.style.overflowX = 'none';
-
-    if(interval){
-        return
-    }
-    interval = setInterval(timer, 1000);
-}
-
-//Function untuk memunculkan halaman profil developer
-function portofolioku() {
-    dataZulhan.style.display = 'flex';
-    main_page.style.display = 'none';
-    biodeveloper.style.display = 'flex';
-    body.style.overflow = 'none';
-}
-
-//Function untuk memunculkan kembali ke halaman beranda
-function backtohome() {
-    dataZulhan.style.display = 'none';
-    main_page.style.display = 'flex';
-    biodeveloper.style.display = 'none';
-    body.style.overflow = 'none';
-}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 // Settingan untuk wallpaper bergerak (Rain Code)
@@ -216,7 +225,7 @@ function handleMouseMove(event) {
     const mouseY = event.clientY;
 
     if (!isMouseMoving) {
-        const delay = 100;
+        const delay = 90;
         createSymbol(mouseX, mouseY, delay);
         isMouseMoving = true;
 
